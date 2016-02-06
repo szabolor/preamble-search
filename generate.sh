dfe6d757b28e3c99ebd31a59e61fa5c8d08bc87f
Szabó Lóránt <szabolor@linux>
2016-02-06 23:02:27 +0100

    shell script tab <-> space issue

--- generate.sh
+++ generate.sh
@@ -5,8 +5,8 @@ THRESHOLD=4
 
 for BITLEN in $(seq -f "%02g" $STARTBITLEN $STOPBITLEN);
 do
-	echo "Executing for bitlen = $BITLEN"
-   ./search_binary $BITLEN $THRESHOLD 0 -1 > output/raw_${BITLEN}bit_${THRESHOLD}thres
+    echo "Executing for bitlen = $BITLEN"
+    ./search_binary $BITLEN $THRESHOLD 0 -1 > output/raw_${BITLEN}bit_${THRESHOLD}thres
     sort -t $'\t' -k 4,4 -n output/raw_${BITLEN}bit_${THRESHOLD}thres > output/sorted_${BITLEN}bit_${THRESHOLD}thres
     echo
 done
